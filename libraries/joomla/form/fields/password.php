f50f1ef1978e86bb78475c781bc7882054f05712
Achal-Aggarwal <theachalaggarwal@gmail.com>
2013-06-24 21:08:09 +0530

    Adding autocomplete attr in calendar, color, email, password and textarea fields.

--- libraries/joomla/form/fields/password.php
+++ libraries/joomla/form/fields/password.php
@@ -52,6 +52,7 @@ class JFormFieldPassword extends JFormField
 		$required   = $this->required ? ' required="required" aria-required="true"' : '';
 		$hint 		= $hint ? ' placeholder="' . $hint . '"' : '';
 		$threshold	= $this->element['threshold'] ? (int) $this->element['threshold'] : 66;
+		$autocomplete = !$this->autocomplete ? ' autocomplete="off"' : '';
 
 		$script = '';
 
@@ -78,7 +79,7 @@ class JFormFieldPassword extends JFormField
 		JHtml::_('script', 'system/html5fallback.js', false, true);
 
 		return '<input type="password" name="' . $this->name . '" id="' . $this->id . '"' .
-			' value="' . htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' .
+		' value="' . htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $hint . $autocomplete . 
 			$auto . $class . $readonly . $disabled . $size . $maxLength . $required . '/>';
 	}
 }
