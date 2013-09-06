88667f99bb989aabfc2a0c4cd72d88175c7f5c66
Achal-Aggarwal <theachalaggarwal@gmail.com>
2013-09-06 17:55:07 +0530

    Refactoring cms form fields.

--- libraries/cms/form/field/menuitem.php
+++ libraries/cms/form/field/menuitem.php
@@ -32,6 +32,90 @@ class JFormFieldMenuitem extends JFormFieldGroupedList
 	public $type = 'MenuItem';
 
 	/**
+	 * The menuType.
+	 *
+	 * @var    string
+	 * @since  3.2
+	 */
+	protected $menuType;
+
+	/**
+	 * The language.
+	 *
+	 * @var    array
+	 * @since  3.2
+	 */
+	protected $language;
+
+	/**
+	 * The published.
+	 *
+	 * @var    array
+	 * @since  3.2
+	 */
+	protected $published;
+
+	/**
+	 * The disable.
+	 *
+	 * @var    array
+	 * @since  3.2
+	 */
+	protected $disable;
+
+	/**
+	 * Method to get certain otherwise inaccessible properties from the form field object.
+	 *
+	 * @param   string  $name  The property name for which to the the value.
+	 *
+	 * @return  mixed  The property value or null.
+	 *
+	 * @since   3.2
+	 */
+	public function __get($name)
+	{
+		switch ($name)
+		{
+			case 'menuType':
+			case 'language':
+			case 'published':
+			case 'disable':
+				return $this->$name;
+		}
+
+		return parent::__get($name);
+	}
+
+	/**
+	 * Method to attach a JForm object to the field.
+	 *
+	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the <field /> tag for the form field object.
+	 * @param   mixed             $value    The form field value to validate.
+	 * @param   string            $group    The field name group control value. This acts as as an array container for the field.
+	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                      full field name would end up being "bar[foo]".
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @see 	JFormField::setup()
+	 * @since   3.2
+	 */
+	public function setup(SimpleXMLElement $element, $value, $group = null)
+	{
+		$result = parent::setup($element, $value, $group);
+
+		if ($result == true)
+		{
+			$this->menuType = (string) $element['menu_type'];
+			$this->published = $element['published'] ? explode(',', (string) $element['published']) : array();
+			$this->disable = $element['disable'] ? explode(',', (string) $element['disable']) : array();
+			$this->language = $element['language'] ? explode(',', (string) $element['language']) : array();
+		}
+
+		return $result;
+	}
+
+	/**
 	 * Method to get the field option groups.
 	 *
 	 * @return  array  The field option objects as a nested array in groups.
@@ -42,14 +126,10 @@ class JFormFieldMenuitem extends JFormFieldGroupedList
 	{
 		$groups = array();
 
-		// Initialize some field attributes.
-		$menuType = (string) $this->element['menu_type'];
-		$published = $this->element['published'] ? explode(',', (string) $this->element['published']) : array();
-		$disable = $this->element['disable'] ? explode(',', (string) $this->element['disable']) : array();
-		$language = $this->element['language'] ? explode(',', (string) $this->element['language']) : array();
+		$menuType = $this->menu_type;
 
 		// Get the menu items.
-		$items = MenusHelper::getMenuLinks($menuType, 0, 0, $published, $language);
+		$items = MenusHelper::getMenuLinks($menuType, 0, 0, $this->published, $this->language);
 
 		// Build group for a specific menu type.
 		if ($menuType)
@@ -60,9 +140,10 @@ class JFormFieldMenuitem extends JFormFieldGroupedList
 			// Build the options array.
 			foreach ($items as $link)
 			{
-				$groups[$menuType][] = JHtml::_('select.option', $link->value, $link->text, 'value', 'text', in_array($link->type, $disable));
+				$groups[$menuType][] = JHtml::_('select.option', $link->value, $link->text, 'value', 'text', in_array($link->type, $this->disable));
 			}
 		}
+
 		// Build groups for all menu types.
 		else
 		{
@@ -77,7 +158,7 @@ class JFormFieldMenuitem extends JFormFieldGroupedList
 				{
 					$groups[$menu->menutype][] = JHtml::_(
 						'select.option', $link->value, $link->text, 'value', 'text',
-						in_array($link->type, $disable)
+						in_array($link->type, $this->disable)
 					);
 				}
 			}
